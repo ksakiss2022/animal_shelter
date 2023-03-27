11c6001f04724f86612e7c8144823e3a82a071b8
LUM <lumlum177@gmail.com>
2023-03-27 16:26:16 +0300

    Push Nikolay with commentary

--- src/main/java/com/example/animal_shelter/animal_shelter/AnimalShelterApplication.java
+++ src/main/java/com/example/animal_shelter/animal_shelter/AnimalShelterApplication.java
@@ -8,6 +8,7 @@ public class AnimalShelterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnimalShelterApplication.class, args);
+        //first commentary by Nikolay (merge feature-nikolay with develop)
     }
 
 }
