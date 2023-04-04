3f67ade73d0247dbdc17d2f774f0fe807100004b
grand <ferthlin@ya.ru>
2023-04-05 00:01:24 +0300

    добавлены api описания

--- src/main/java/com/example/animal_shelter/animal_shelter/controller/DogController.java
+++ src/main/java/com/example/animal_shelter/animal_shelter/controller/DogController.java
@@ -1,14 +1,18 @@
 package com.example.animal_shelter.animal_shelter.controller;
 
-import com.example.animal_shelter.animal_shelter.model.Cat;
 import com.example.animal_shelter.animal_shelter.model.Dog;
-import com.example.animal_shelter.animal_shelter.service.CatService;
 import com.example.animal_shelter.animal_shelter.service.DogService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 /**
- * Контроллер DogController предназначен для вывода дынных собак
- * с использованием методов прописанных в DogService.
+ * <b>Контроллер DogController предназначен для вывода дынных собак
+ * с использованием методов прописанных в DogService.</b>
  * Реализованы POST, PUT, DELETE запросы.
  * В дальнейшем будут добавлено множество других удобных запросов, для работы с
  * информацией о собаках и их владельцах.
@@ -21,17 +25,45 @@ public class DogController {
     public DogController(DogService dogService) {
         this.dogService = dogService;
     }
-
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Вносим информацию о новой собаке " +
+                            "пример вносимой информации:.......... ",
+                    content = @Content(
+                            schema = @Schema(implementation = Dog[].class),
+                            examples = @ExampleObject(externalValue = ".......допишем позже")
+                    )
+            )
+    })
     @PostMapping //POST http://localhost:8080/dogs
     public Dog createDog(@RequestBody Dog dog) {
         return dogService.createDod(dog);
     }
-
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Изменяе ранее внесенную информацию о собаке ",
+                    content = @Content(
+                            schema = @Schema(implementation = Dog[].class),
+                            examples = @ExampleObject(externalValue = ".......допишем позже")
+                    )
+            )
+    })
     @PutMapping //PUT http://localhost:8080/dogs
     public Dog editDog(@RequestBody Dog dog) {
         return dogService.editDog(dog);
     }
-
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Удаляем ранее внесенную информацию о собаке ",
+                    content = @Content(
+                            schema = @Schema(implementation = Dog[].class),
+                            examples = @ExampleObject(externalValue = ".......допишем позже")
+                    )
+            )
+    })
     @DeleteMapping("{id}") //DELETE http://localhost:8080/dogs/3
     public ResponseEntity deleteDog(@PathVariable Long id) {
         dogService.deleteDog(id);
